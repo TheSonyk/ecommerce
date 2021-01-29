0fe421eca49fb80b21b309f16c23a08c13a50115
BuildTools <unconfigured@null.spigotmc.org>
2021-01-29 15:51:20 +0100

    C bon

---
+++ navbar.php
@@ -0,0 +1,54 @@
+  <!-- Navbar -->
+  <nav class="navbar fixed-top navbar-expand-lg navbar-light white scrolling-navbar">
+    <div class="container">
+
+      <!-- Brand -->
+      <a class="navbar-brand waves-effect" href="index.php">
+        <strong class="green-text">Eco-Service</strong>
+      </a>
+
+      <!-- Collapse -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- Links -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <!-- Left -->
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link waves-effect" href="infos.php" target="_blank">A propos du site</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link waves-effect" href="contact.php"
+              target="_blank">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link waves-effect" href="newsletter.php" target="_blank">Newsletter</a>
+          </li>
+        </ul>
+
+        <!-- Right -->
+        <ul class="navbar-nav nav-flex-icons">
+          <li class="nav-item">
+            <a class="nav-link waves-effect">
+              <span class="badge red z-depth-1 mr-1"> 1 </span>
+              <i class="fas fa-shopping-cart"></i>
+              <span class="clearfix d-none d-sm-inline-block"> Panier </span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="connexion.php" class="nav-link border border-light rounded waves-effect">Se Connecter</a>
+          </li>
+          <li class="nav-item">
+            <a href="inscription.php" class="nav-link border border-light rounded waves-effect">Créer un compte</a>
+          </li>
+        </ul>
+
+      </div>
+
+    </div>
+  </nav>
+  <!-- Navbar -->
